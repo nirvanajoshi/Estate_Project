bc2bb04a4144d776515d74c84224b4d883ea3643
nirvanajoshi <damodarj36@gmail.com>
2026-07-27 11:58:19 +0545

    Admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Agent, PropertyType, Listing, Favorite, Inquiry, ListingImage
 
-# Register your models here.
+admin.site.register(Agent)
+admin.site.register(PropertyType)
+admin.site.register(Listing)
+admin.site.register(Favorite)
+admin.site.register(Inquiry)
+admin.site.register(ListingImage)
